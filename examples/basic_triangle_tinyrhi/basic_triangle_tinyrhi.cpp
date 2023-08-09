8ce428acaa31f2be4e4a3a99b9eb5a3eafc00ae5
dongqing <dongqing.czd@xuelangyun.com>
2023-08-09 20:55:16 +0800

    add shader module creation

--- examples/basic_triangle_tinyrhi/basic_triangle_tinyrhi.cpp
+++ examples/basic_triangle_tinyrhi/basic_triangle_tinyrhi.cpp
@@ -30,6 +30,7 @@
 #include <vulkan/vulkan.h>
 #include <GLFW/glfw3.h>
 #include <set>
+#include <fstream>
 
 #ifdef NDEBUG
 const bool enableValidationLayers = false;
@@ -44,6 +45,23 @@ const std::vector<const char*> validationLayers = {
 const std::vector<const char*> deviceExtensions = {
     VK_KHR_SWAPCHAIN_EXTENSION_NAME
 };
+
+static std::vector<uint8_t> readFile(const std::string& filename)
+{
+    std::ifstream file(filename, std::ios::ate | std::ios::binary);
+    if (!file.is_open())
+    {
+        throw std::runtime_error("failed to open file !");
+    }
+
+    size_t fileSize = (size_t)file.tellg();
+    std::vector<uint8_t> buffer(fileSize);
+    file.seekg(0);
+    file.read((char*)buffer.data(), fileSize);
+    file.close();
+
+    return buffer;
+}
 
 // Wrap Vk function
 VkResult CreateDebugUtilsMessagerExt(VkInstance instance,
@@ -487,7 +505,46 @@ public:
 
     void createGraphicsPipeline()
     {
+        auto vertShaderCode = readFile("../examples/basic_triangle_tinyrhi/shaders/vert.spv");
+        auto fragShaderCode = readFile("../examples/basic_triangle_tinyrhi/shaders/frag.spv");
 
+        VkShaderModule vertShaderModule = createShaderModule(vertShaderCode);
+        VkShaderModule fragShaderModule = createShaderModule(fragShaderCode);
+
+        VkPipelineShaderStageCreateInfo vertShaderStageInfo = {};
+        vertShaderStageInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+        vertShaderStageInfo.stage = VK_SHADER_STAGE_VERTEX_BIT;
+        vertShaderStageInfo.module = vertShaderModule;
+        vertShaderStageInfo.pName = "main";
+
+		VkPipelineShaderStageCreateInfo fragShaderStageInfo = {};
+        fragShaderStageInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+        fragShaderStageInfo.stage = VK_SHADER_STAGE_FRAGMENT_BIT;
+        fragShaderStageInfo.module = fragShaderModule;
+        fragShaderStageInfo.pName = "main";
+
+        VkPipelineShaderStageCreateInfo shaderStages[] = { vertShaderStageInfo,
+            fragShaderStageInfo
+        };
+
+
+        vkDestroyShaderModule(device, fragShaderModule, nullptr);
+        vkDestroyShaderModule(device, vertShaderModule, nullptr);
+    }
+
+    VkShaderModule createShaderModule(const std::vector<uint8_t>& code)
+    {
+        VkShaderModuleCreateInfo createInfo = {};
+        createInfo.sType = VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO;
+        createInfo.pCode = reinterpret_cast<const uint32_t*>(code.data());
+
+        VkShaderModule shaderModule;
+        if (vkCreateShaderModule(device, &createInfo, nullptr, &shaderModule) != VK_SUCCESS)
+        {
+            throw std::runtime_error("failed to create shader module!");
+        }
+
+        return shaderModule;
     }
 private:
     VkInstance instance;
